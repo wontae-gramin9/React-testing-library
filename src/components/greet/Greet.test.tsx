dda7ba3f069c3b5e77a539e15fedf9831c5e2db3
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-16 13:38:44 +0000

    'it', 'test' interchangable. test.only → fit, test.skip→ xit

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,16 +1,23 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-// skips this test
-test.skip('Greet renders correctly', () => {
-  render(<Greet/>)
-  const textElement = screen.getByText(/hello/i)
-  expect(textElement).toBeInTheDocument()
-})
+// it이든 test든 상관없이 사용가능하다
+describe('Greet', () => {
+  // test.only
+  fit('Greet renders correctly', () => { 
+    render(<Greet/>)
+    const textElement = screen.getByText(/hello/i)
+    expect(textElement).toBeInTheDocument()
+  })
+  
+  describe('Nested describe', () => {
+    
+    // test.skip
+    xit('Greet renders with a name', () => {
+      render(<Greet name='wontae' />)
+      const textElement = screen.getByText('Hello wontae')
+      expect(textElement).toBeInTheDocument()
+    })
+  })
 
-// Only runs this test
-test.only('Greet renders with a name', () => {
-  render(<Greet name='wontae' />)
-  const textElement = screen.getByText('Hello wontae')
-  expect(textElement).toBeInTheDocument()
-})
+}) 
