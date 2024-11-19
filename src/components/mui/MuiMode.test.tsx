9a8099766a32214527f81388c1dab7f88d6c7b32
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-19 14:49:39 +0000

    [Provider] {wrapper: Provider} option을 단 custom Render를 test-utils.tsx에 export해서 사용하면, 매번 option을 넣어주지 않아도 된다.

--- src/components/mui/MuiMode.test.tsx
+++ src/components/mui/MuiMode.test.tsx
@@ -1,14 +1,9 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "../../test-utils"
 import MuiMode from "./MuiMode"
-import AppProviders from "../../providers/AppProviders"
 
 describe('MuiMode', () => {
   test('renders properly', () => { 
-    // As we are not importing App but MuiMode, wrapped by a provider
-    // we need to wrap it
-    render(<MuiMode/>, {
-      wrapper: AppProviders
-    })  
+    render(<MuiMode/>)  
     const headingElement = screen.getByRole('heading')
     expect(headingElement).toHaveTextContent(/dark mode/i)
   })
---
+++ src/test-utils.tsx
@@ -0,0 +1,12 @@
+// https://testing-library.com/docs/react-testing-library/setup#custom-render
+import { ReactElement } from 'react'
+import {render, RenderOptions} from '@testing-library/react'
+import AppProviders from './providers/AppProviders'
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, {wrapper: AppProviders, ...options})
+
+export * from '@testing-library/react'
+export {customRender as render}
